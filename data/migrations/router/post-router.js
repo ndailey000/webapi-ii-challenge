347b43461db7ab8eaa77d643cb3edff7005a1b1f
Nick Dailey <n.dailey000@icloud.com>
2019-11-04 04:31:50 -0600

    end of day.

--- data/migrations/router/post-router.js
+++ data/migrations/router/post-router.js
@@ -1,13 +1,13 @@
 const express = require('express');
 // this brings in express
-const Post= require('../db.js');
+const Post= require('./db.js');
 // this brings in the H db
 const router = express.Router();
 // this allows us to create a mini server. ().listen will not work.
 
 
 
-router.get('/', async (req, res) => {
+router.get('/api/post', async (req, res) => {
     try{
         const post = await Post.find(req.query);
         // would i iter. with a for loop here if ther is no data in the array?
@@ -21,25 +21,8 @@ router.get('/', async (req, res) => {
         }
     
 });
-// you cant put the entire url call on this or it will need you to enter 
-// it 2 time
-router.get('/:id', (req, res) => {
-    Post.findById(req.params.id)
-    .then(post => {
-      if (post) {
-        res.status(200).json(hub);
-      } else {
-        res.status(404).json({ message: 'Post not found' });
-      }
-    })
-    .catch(error => {
-      // log error to database
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the post by ID',
-      });
-    });
-  });
+
+
 
 
 module.exports = router;
